9848c51f531a5ba5939ada653fd1ac682459d5d0
albert234567 <albert.roig1@insbaixcamp.cat>
2025-10-17 13:34:46 +0200

    mostrar email al dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="flex items-center justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+
+            {{-- Nom i email de l’usuari autenticat --}}
+            @if (Auth::check())
+                <div class="text-right">
+                    <p class="text-sm text-gray-500">{{ Auth::user()->email }}</p>
+                </div>
+            @endif
+        </div>
     </x-slot>
 
     <div class="py-12">
